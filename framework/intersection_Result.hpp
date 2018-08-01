7627c3b91fab8a35a2241ec532e1822122541bfc
caixiaoniweimar <710389740@qq.com>
2018-08-01 22:54:25 +0200

    Add konstruktur

--- framework/intersection_Result.hpp
+++ framework/intersection_Result.hpp
@@ -4,9 +4,21 @@
 // um Result (von Objekt mit Ray Intersection) zu speichern
 struct intersectionResult{
 	float distance;
-	bool hit=false;
-	glm::vec3 position={0,0,0}; 
-	glm::vec3 normal={0,0,0}; 
+	bool hit;
+	glm::vec3 position; 
+	glm::vec3 normal;
+	//shared_ptr<Shape> closest_shape;
+
+	intersectionResult():
+		distance{0.0},
+		hit{false},
+		position{0,0,0},
+		normal{0,0,0} {}
+	intersectionResult( intersectionResult const& result):
+	    distance{result.distance},
+	    hit{result.hit},
+	    position{result.position},
+	    normal{result.normal} {}
 };
 
 #endif
