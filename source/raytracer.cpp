a4c5e3fae7870f868d3002be67769fd0d818dad2
caixiaoniweimar <710389740@qq.com>
2018-08-01 22:56:19 +0200

    loeschen alte Kommentar

--- source/raytracer.cpp
+++ source/raytracer.cpp
@@ -25,34 +25,10 @@ int main(int argc, char* argv[])
   //create separate thread to see updates of pixels while rendering 创建单独的线程以在渲染时查看像素的更新
   //std::thread render_thread([&renderer]() {renderer.render();});
 
-  //Material red{};
-  //Sphere sphere1{glm::vec3{0,10,-10},5};
-  //Light light{"sun",{50,-40,10},{1,1,1},1};
-  //Camera camera{};
-  //Color ambiente{0.0f,0.0f,0.0f};
-  //Light light{"sun",{1,1,1},{1,1,1},10};
-  //Sphere sphere1{glm::vec3{0,5,-10},5.0};
-
-  //renderer.render1(camera,sphere1);
-  //renderer.render1(camera,red,sphere1,light,ambiente);
-  //renderer.render1(camera,red,sphere1,light);
   Scene scene{};
 
-  //多个物体,一盏灯
-  //Sphere sphere_1{ {-2,5,-2}, 4.0 };
-  //Sphere sphere_2{ {5,5,-7}, 3.0 };
-
-
-  //Material material_1 { "red", {1,0,0},{1,0,0},{1,1,1},16};
-  //Material material_2 { "blue",{0,0,1},{0,0,1},{1,1,1},16};
   scene=open_sdf_datei("beispiel.xsd");
-  renderer.render1(scene);
-  //sphere_1.material_ {material_1};
-  //sphere_2.material_ {material_2};
-  //vector<shared_ptr<Shape>> objekt = scene.container_objekt;
-  //objekt.push_back(sphere_1);
-  //cout<<sphere_1->name_<<endl;
-  //scene.container_objekt.push_back(sphere_2);
+  renderer.render(scene);
 
   Window window{{image_width, image_height}};
 
