8bbfc630003fd0d1d111be7542ae9d432dbf0a79
caixiaoniweimar <710389740@qq.com>
2018-08-02 22:33:33 +0200

    Add shape Methode, getNormal virtual

--- framework/shape.hpp
+++ framework/shape.hpp
@@ -6,9 +6,11 @@
 #include "color.hpp"
 #include "ray.hpp"
 #include "material.hpp"
-#include "intersection_Result.hpp"
+#include <glm/glm.hpp>
+
 using namespace std;    
-  
+
+struct intersectionResult;
 class Shape{
 public:
 // Aufgabe 5.3
@@ -17,12 +19,10 @@ public:
 		Shape(string const& name);
 		Shape(string const& name, shared_ptr<Material> const& material);
 		virtual ~Shape();
-	    //~Shape();
 		// Destruktoren legt man nur an wenn sie Sekundärressourcen freigeben muessen 
-
 // Aufgabe 5.1
-		virtual double area() const=0;  // 在 .hpp 写virtual; 在 .cpp 不需要写
-		virtual double volume() const=0; // Wir koennen kein statisches/dynamsiches Objekt von Shape anlegen
+		//virtual double area() const=0;  // 在 .hpp 写virtual; 在 .cpp 不需要写
+		//virtual double volume() const=0; // Wir koennen kein statisches/dynamsiches Objekt von Shape anlegen
 										// sondern nur ein Pointer/Referenz
 
 // Aufgabe 5.4
@@ -31,6 +31,7 @@ public:
 // Aufgabe 6.3
 		virtual bool intersect (Ray const& ray, float& t) const=0;
 		virtual intersectionResult istIntersect(Ray const& ray,float& t) const=0;
+		virtual glm::vec3 getNormal(intersectionResult const& schnittpunkt) const=0;
 
 		void set_name(string const& name);
 		string get_name() const;
@@ -39,13 +40,39 @@ public:
 // abgleiteted Klasse wird diese Attribute aufrufen( vererben diese Attribute als private )
 		string name_;
 // Aufgabe 6.4
-
 		shared_ptr<Material> material_;
-
-	
 };
-
 // Aufgabe 5.4
 ostream& operator<<(ostream& os,Shape const& s);
+struct intersectionResult{
+	float distance;
+	bool hit;
+	glm::vec3 position; 
+	glm::vec3 normal;
+	//shared_ptr<Shape> closest_shape;
+    Shape const* closest_shape;  // ?!!!!!
+	// sphere.cpp result.closest_shape = this !! Fehler! (wenn kein *)
 
+	intersectionResult():
+		distance{2000.0},
+		hit{false},
+		position{0,0,0},
+		normal{0,0,0}, 
+		closest_shape{nullptr}
+		{}
+	intersectionResult( intersectionResult const& result):
+	    distance{result.distance},
+	    hit{result.hit},
+	    position{result.position},
+	    normal{result.normal}, 
+		closest_shape{result.closest_shape}
+		{}
+	intersectionResult(Shape const* closest_shape_):
+	    distance{2000.0},
+	    hit{false},
+	    position{0,0,0},
+	    normal{0,0,0}, 
+		closest_shape{closest_shape_}
+		{}
+};
 #endif
