ed1d116a1791d496ee35b792ac1079ec858d3d60
caixiaoniweimar <710389740@qq.com>
2018-08-03 13:14:52 +0200

    Add Ray konstruktur

--- framework/ray.hpp
+++ framework/ray.hpp
@@ -9,6 +9,10 @@ struct Ray{
 	glm::vec3 direction ={0.0f,0.0f,-1.0f};
 	//glm::vec3 punkt;-> getpoint(t)
 	
+	Ray():
+		origin{0.0f,0.0f,0.0f},
+		direction{1.0f,0.0f,0.0f}{}
+
 	Ray(glm::vec3 const& origin_, glm::vec3 const& direction_):
 	origin{origin_},
 	direction{ glm::normalize(direction_) } {}
@@ -16,16 +20,7 @@ struct Ray{
 	glm::vec3 getpoint(float t) const{
 		return origin + direction*t;
 	}
-
-
 };
-/*Ray transformRay(glm::mat4 const& matrix, Ray const& ray){
-	glm::vec4 a{ray.origin,1.0f};
-	glm::vec4 b{ray.direction,0.0f};
-	glm::vec3 origin(matrix*a);
-	glm::vec3 direction(matrix*b);
-	return {origin,direction};
-}*/
 
 
 
